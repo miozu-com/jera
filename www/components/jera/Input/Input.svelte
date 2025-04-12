7054b14470720cbdcdb6c17c4611d13fc9617dfb
ng <glazer.nicholas@gmail.com>
2025-04-12 05:03:09 +0000

    initial

---
+++ www/components/jera/Input/Input.svelte
@@ -0,0 +1,63 @@
+<script>
+  let {
+    value = '',
+    ref = $bindable(),
+    type = 'text',
+    placeholder = '',
+    disabled = false,
+    required = false,
+    name = '',
+    id = '',
+    autocomplete = 'on',
+    autocorrect = 'off',
+    autocapitalize = 'off',
+    spellcheck = 'false',
+    maxlength = undefined,
+    minlength = undefined,
+    inputmode,
+    class: className = '',
+    unstyled = false,
+    disableBrowserFeatures = false,
+    oninput = () => {},
+    onchange = () => {},
+    onkeydown = () => {},
+    ...others
+  } = $props();
+
+  const finalAutocomplete = disableBrowserFeatures ? 'new-password' : autocomplete;
+  const finalClassName = $derived(unstyled ? className : `base-input ${className}`);
+</script>
+
+<input
+  class={finalClassName}
+  bind:this={ref}
+  {id}
+  {name}
+  {type}
+  {value}
+  {placeholder}
+  {disabled}
+  {required}
+  {inputmode}
+  {maxlength}
+  {minlength}
+  autocomplete={finalAutocomplete}
+  autocorrect={disableBrowserFeatures ? 'off' : autocorrect}
+  autocapitalize={disableBrowserFeatures ? 'off' : autocapitalize}
+  spellcheck={disableBrowserFeatures ? 'false' : spellcheck}
+  data-form-type={disableBrowserFeatures ? 'other' : undefined}
+  data-lpignore={disableBrowserFeatures ? 'true' : undefined}
+  {oninput}
+  {onchange}
+  {onkeydown}
+  {...others}
+/>
+
+<style>
+  .input-wrapper {
+    @apply relative;
+  }
+  .input-label {
+    @apply font-bold inline-block;
+  }
+</style>
